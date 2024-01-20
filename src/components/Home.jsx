ff1a5ee17593146404dc7ca78724421abe096b71
Pratyush Kumar <pratyushkumardelhi55@gmail.com>
2024-01-21 02:47:40 +0530

    new page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,8 @@ import waves from '../assets/waves.png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
+import ProductDetail from './ProductDetail';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -15,47 +17,52 @@ const Home = () => {
         <h1> PRICE MATTERS </h1>
         <form className="search">
           <input type="text" placeholder='Type Your Product Name..' />
-          <button>Search</button>
+          <button><Link className='prod' to="/productdetails">Search</Link></button>
         </form>
       </section>
       <section className="popular">
-        <div className="popularBeginStyle">
+        <div className="division">
+          <div className="popularBeginStyle">
           <FontAwesomeIcon icon={faAnglesDown} />
         </div>
         <img src={waves} alt="waves" />
-        <h1>Popular Searches</h1>
-        <div className="popularCards">
-          <div className="iphone15pro">
-            <img src="" alt="iphone" />
-            <h2>Iphone 15 pro max</h2>
-            <span>1TB</span>
-            <span>|</span>
-            <span>Titanium</span>
-            <button>Compare Price</button>
-          </div>
-          <div className="twoSmall">
-            <div className="marshal">
-              <img src="" alt="marshal"></img>
-              <h3>Marshal</h3>
-              <p>`Major IV Wireless Bluetooth On Ear Headphone with Mic $(Black)`</p>
+        </div>
+        <div className="popularBegin">
+          <h1>Popular Searches</h1>
+          <div className="popularCards">
+            <div className="iphone15pro">
+              <img src="" alt="iphone" />
+              <h2>Iphone 15 pro max</h2>
+              <span>1TB</span>
+              <span>|</span>
+              <span>Titanium</span>
               <button>Compare Price</button>
             </div>
-            <div className="jbl">
-            <img src="" alt="marshal"></img>
-              <h3>JBL</h3>
+            <div className="twoSmall">
+              <div className="marshal">
+                <img src="" alt="marshal"></img>
+                <h3>Marshal</h3>
+                <p>`Major IV Wireless Bluetooth On Ear Headphone with Mic $(Black)`</p>
+                <button>Compare Price</button>
+              </div>
+              <div className="jbl">
               <img src="" alt="marshal"></img>
-              <h3>Marshal</h3>
-              <p>`Major IV Wireless Bluetooth On Ear Headphone with Mic $(Black)`</p>
+                <h3>JBL</h3>
+                <img src="" alt="marshal"></img>
+                <h3>Marshal</h3>
+                <p>`Major IV Wireless Bluetooth On Ear Headphone with Mic $(Black)`</p>
+                <button>Compare Price</button>
+              </div>
+            </div>
+            <div className='Samsung Smart TV'>
+              <img src="" alt="TV"></img>
+              <h2>Samsung Smart TV</h2>
+              <p>`1m 63cm $(65") Q70A QLED 4K Smart TV`</p>
               <button>Compare Price</button>
             </div>
-          </div>
-          <div className='Samsung Smart TV'>
-            <img src="" alt="TV"></img>
-            <h2>Samsung Smart TV</h2>
-            <p>`1m 63cm $(65") Q70A QLED 4K Smart TV`</p>
-            <button>Compare Price</button>
           </div>
         </div>
+          
       </section>
 
     </main>
---
+++ src/components/ProductDetail.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/productdetail.scss'
+
+const ProductDetail = () => {
+  return (
+    <div>ProductDetail</div>
+  )
+}
+
+export default ProductDetail
